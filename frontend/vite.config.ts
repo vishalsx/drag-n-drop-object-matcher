27189da2b22a65891ef48e7f65c943a26c8205f1
vishal-alphatub <vishal.saxena@alphatub.com>
2025-09-25 09:41:22 +0530

    Pointing to render server..

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
       // Proxies API requests to the backend server during development.
       // For example, a request to /api/pictures will be forwarded to http://localhost:8000/pictures
       '/api': {
-        target: 'http://localhost:8080', // Your backend server
+        target: 'https://drag-n-drop-object-matcher.onrender.com', // Your backend server
         changeOrigin: true, // Needed for virtual hosted sites
         rewrite: (path) => path.replace(/^\/api/, ''), // Removes /api from the start of the path
       },
